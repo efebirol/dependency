ee09b6411253c33670a7e969f6f3c6fa79bd0453
Birol <tw04n202@gmail.com>
2019-04-15 00:31:56 +0200

    test go green

--- inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,7 +1,10 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import com.mhp.coding.challenges.dependency.notifications.EmailHandler;
+import com.mhp.coding.challenges.dependency.notifications.PushNotificationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +12,14 @@ public class InquiryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(InquiryService.class);
 
+    @Autowired
+    EmailHandler emailHandler;
+    @Autowired
+    PushNotificationHandler pushNotificationHandler;
+
     public void create(final Inquiry inquiry) {
+        emailHandler.sendEmail(inquiry);
+        pushNotificationHandler.sendNotification(inquiry);
         LOG.info("User sent inquiry: {}", inquiry);
     }
 
